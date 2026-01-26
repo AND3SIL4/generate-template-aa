1e59503dbf6e3ed7e5492ff0cce9f00ba5e675bb
AND3SIL4 <devaul.fs@gmail.com>
2026-01-26 12:28:03 -0500

    🌟 feat(app): add the updater plugin to the entire application
    Changelog: feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css"
+import "./App.css";
+import { useEffect } from "react";
+import { check } from "@tauri-apps/plugin-updater"
 
 
 function App() {
+  useEffect(() => {
+    async function runUpdaterCheck() {
+      try {
+        const update = await check()
+        if (update) {
+          console.log(
+            `Found a new version ${update.version} from ${update.date} with notes ${update.body}`
+          );
+
+          await update.downloadAndInstall((event) => {
+            switch (event) {
+              case "Started":
+                contentLength = event.data.contentLength;
+                console.log(`Started downloading ${contentLength} bytes`);
+                break;
+              case "Progress":
+                downloaded += event.data.chunkLenght;
+                console.log(`Downloaded ${downloaded} from ${contentLength}`);
+                break;
+              case "Finished":
+                console.log("Download finished");
+                break;
+              default:
+                break;
+            }
+          })
+
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    runUpdaterCheck()
+  }, [])
+
+
   return (
-    <h1 className="text-yellow-600">Byaas project</h1>
+    <h1 className="text-yellow-600">Byaas project v0.1.2</h1>
   )
 }
 
