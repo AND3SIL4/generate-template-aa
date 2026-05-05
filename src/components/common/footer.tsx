4a8f816da61223f103a575818121ba4bdf4b908a
AND3SIL4 <devaul.fs@gmail.com>
2026-05-04 23:22:02 -0500

    🌟 feat: add footer component and information
    Changelog: feature

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,5 +1,18 @@
 const Footer = () => {
-  return <footer>This is the footer</footer>;
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="bg-slate-200 p-3 text-center flex w-full items-center justify-center">
+      Developed by
+      <a
+        className="p-1 font-medium animate-pulse hover:text-green-800"
+        href="https:github.com/and3sil4"
+        target="blank"
+      >
+        @AND3SIL4
+      </a>
+      - {currentYear}
+    </footer>
+  );
 };
 
 export default Footer;
