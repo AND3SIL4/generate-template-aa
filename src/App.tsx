abf1cfb1a711a365471b5fe913762423321378e0
AND3SIL4 <devaul.fs@gmail.com>
2026-05-04 23:21:47 -0500

    🌟 feat: structure the components better
    Changelog: feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 import Footer from './components/common/footer';
+import MainForm from './components/common/mainForm';
 import Navbar from './components/navigation/navbar';
 
 const App = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
+      <MainForm />
       <Footer />
-    </>
+    </div>
   );
 };
 
