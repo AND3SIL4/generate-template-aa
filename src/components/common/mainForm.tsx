dc5e178e76461312db050e3a3c6e5fe7b70f9770
AND3SIL4 <devaul.fs@gmail.com>
2026-06-19 16:43:05 -0500

    (app): remote no needed files and restart from scratch

--- src/components/common/mainForm.tsx
+++
@@ -1,9 +0,0 @@
-const MainForm = () => {
-  return (
-    <main className="flex-1">
-      <h1>Main Form</h1>
-    </main>
-  );
-};
-
-export default MainForm;
--- src/utils/types.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export type ThemeVariant = 'cyber' | 'neon' | 'sunset' | 'vice' | 'gold';
-
-export type ButtonType = {
-  children: React.ReactNode;
-  className?: string;
-  size: 'sm' | 'md' | 'lg';
-};
