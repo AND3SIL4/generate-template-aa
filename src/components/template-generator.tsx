201ace4ccf42ae60e920eb05f7e9dc2b155ebdfa
AND3SIL4 <devaul.fs@gmail.com>
2026-06-22 10:41:11 -0500

    build(compoments): main components created (documentation, history, generator)

---
+++ src/components/template-generator.tsx
@@ -0,0 +1,5 @@
+const TemplateGenerator = () => {
+  return <h1>Template Generator</h1>;
+};
+
+export default TemplateGenerator;
---
+++ src/components/template-history.tsx
@@ -0,0 +1,4 @@
+const TemplateHistory = () => {
+  return <h1>Template History</h1>
+}
+export default TemplateHistory;
