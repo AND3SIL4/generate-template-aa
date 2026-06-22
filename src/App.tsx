cb3b67962c9ca284ba8568b4d8d3de383893461d
AND3SIL4 <devaul.fs@gmail.com>
2026-06-22 13:43:09 -0500

    configure sonner tool to standarize app style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import HomePage from "./pages/home-page";
 import "./styles/global.css"
+import { Toaster } from "sonner";
 
 const App = () => {
-  return <HomePage />
+  return (
+    <>
+      <Toaster richColors position="bottom-center" closeButton />
+      <HomePage />
+    </>
+  )
 }
 
 
