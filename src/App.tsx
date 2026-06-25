e134e6a74f77f101843ca8eba06f3e7e75e4abf6
AND3SIL4 <devaul.fs@gmail.com>
2026-06-24 22:15:27 -0500

    update(app.tsx): apply code format

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import HomePage from "./pages/home-page";
-import "./styles/global.css"
 import { Toaster } from "sonner";
+import HomePage from "./pages/home-page";
+import "./styles/global.css";
 
 const App = () => {
   return (
@@ -8,8 +8,7 @@ const App = () => {
       <Toaster richColors position="bottom-center" closeButton />
       <HomePage />
     </>
-  )
-}
-
+  );
+};
 
 export default App;
