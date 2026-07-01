970dc3efa8e39de48b3d637a7766808c26628103
AND3SIL4 <devaul.fs@gmail.com>
2026-07-01 11:34:41 -0500

    update(command): scaffold command updated turn it into async function and call the insertion in database

--- src-tauri/src/commands/scaffold.rs
+++ src-tauri/src/commands/scaffold.rs
@@ -1,23 +1,24 @@
 use std::{env::temp_dir, fs, path::Path};
 
 use directories::UserDirs;
+use sqlx::{Pool, Postgres};
 
 use crate::domain::{
     constants::{CURRENT_TEMPLATE_NAME, TEMP_FOLDER},
     implementations::{ScaffoldData, ScaffoldResponse},
 };
 use crate::infra::zipper;
-use crate::services::{generate_scaffold, github_releases};
+use crate::services::{self, generate_scaffold, github_releases};
 
 #[tauri::command]
-pub fn generate_template(scaffold_data: ScaffoldData) -> Result<ScaffoldResponse, String> {
+pub async fn generate_template(scaffold_data: ScaffoldData) -> Result<ScaffoldResponse, String> {
     // Create the temp folder path to make all activities
     let mut temp_folder = temp_dir(); // Create base directory
     temp_folder.push(TEMP_FOLDER); // Add a temp folder to isolate the activities
     fs::create_dir_all(&temp_folder).map_err(|e| e.to_string())?;
 
     let template_file =
-        github_releases::download_current_template(&temp_folder, &scaffold_data.customer)?;
+        github_releases::download_current_template(&temp_folder, &scaffold_data.customer).await?;
     zipper::unzipfile(&template_file, &temp_folder)?; // Unzip downloaded file
     fs::remove_file(&template_file).map_err(|e| e.to_string())?; // Remove after extract all the content
 
@@ -67,7 +68,24 @@ pub fn generate_template(scaffold_data: ScaffoldData) -> Result<ScaffoldResponse
         }
     }
 
+    // Warp up the template creation
     zipper::zipfile(&temp_folder, Path::new(&final_file))?; // Final result
     fs::remove_dir_all(&temp_folder).map_err(|e| e.to_string())?;
-    Ok(ScaffoldResponse::success(&final_file, &total_matches))
+
+    // Try to insert the record in database (Optional)
+    // Insert record in database after generate template
+
+    let pool: Pool<Postgres> = services::database::get_database_connection()
+        .await
+        .expect("Couldn't connect to database");
+
+    let id_template = services::database::insert_record(scaffold_data, &pool)
+        .await
+        .expect("An error occurre when generate a template");
+
+    Ok(ScaffoldResponse::success(
+        &final_file,
+        &total_matches,
+        &id_template,
+    ))
 }
